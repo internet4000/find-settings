32e7bab1d87c6125c6f96f1de396f83230518aa3
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-12-07 18:53:07 +0100

    initial split

---
+++ src/EnginesList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Engine from './Engine'
+
+var EnginesList = ({engines, onDelete, canDelete}) => (
+	<div className="EngineList">
+		{
+			Object.keys(engines)
+				.map(id => <Engine
+											 id={id}
+											 url={engines[id]}
+											 handleClick={onDelete}
+											 handlesClick={canDelete}/> )
+		}
+	</div>
+)
+
+export default EnginesList
